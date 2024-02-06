d26d44043f613faebc28ec64d7d7ba4306ec2e88
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-02-06 20:10:56 +0530

    add sol.

---
+++ 18 Ass- JS Fund. 2/Q3.js
@@ -0,0 +1,4 @@
+let number = -2;
+
+let result = number > 0 ? "Number is positive" : number === 0 ? "Number is 0" : "Number is negative";
+console.log(result)
---
+++ 18 Ass- JS Fund. 2/Q4.js
@@ -0,0 +1,5 @@
+let age = 15    ;
+let  message= age>=18 ? "You can VOTE" : "You cannot VOTE" ;
+console.log(message)
+
+// Output : You cannot VOTE 
